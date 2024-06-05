a6d0c6510173277e69c476fd40e123d8a650be6b
unknown <autumnteo96@gmail.com>
2024-06-05 22:44:56 +0800

    add reddit & twitter pipelines

--- socialMediaPipeline/social_media_pipeline.py
+++ socialMediaPipeline/social_media_pipeline.py
@@ -78,6 +78,226 @@ class SocialMediaPipeline(ABC):
         pass
 
 
+class RedditPipeline(SocialMediaPipeline):
+    @log_metadata
+    def extract(
+        self,
+        id: str,
+        num_records: int,
+        client: praw.Reddit,
+    ) -> List[SocialMediaData]:
+
+        logging.info('Extracting reddit data.')
+        if client is None:
+            raise ValueError(
+                'reddit object is None. Please pass a valid praw.Reddit'
+                ' object.'
+            )
+
+        subreddit = client.subreddit(id)
+        top_subreddit = subreddit.hot(limit=num_records)
+        reddit_data = []
+        for submission in top_subreddit:
+            reddit_data.append(
+                SocialMediaData(
+                    id=submission.id,
+                    source='reddit',
+                    social_data=RedditPostData(
+                        title=submission.title,
+                        score=submission.score,
+                        url=submission.url,
+                        comms_num=submission.num_comments,
+                        created=str(submission.created),
+                        text=submission.selftext,
+                    ),
+                )
+            )
+        return reddit_data
+
+    @log_metadata
+    def transform(
+        self,
+        social_data: List[SocialMediaData],
+        transform_function: Callable[
+            [List[SocialMediaData]], List[SocialMediaData]
+        ],
+    ) -> List[SocialMediaData]:
+
+        logging.info('Transforming reddit data.')
+        return transform_function(social_data)
+
+    @log_metadata
+    def load(
+        self,
+        social_data: List[SocialMediaData],
+        db_cursor_context: DatabaseConnection,
+    ) -> None:
+        logging.info('Loading reddit data.')
+        if db_cursor_context is None:
+            raise ValueError(
+                'db_cursor is None. Please pass a valid DatabaseConnection'
+                ' object.'
+            )
+
+        with db_cursor_context as cur:
+            for post in social_data:
+                cur.execute(
+                    """
+                    INSERT OR REPLACE INTO social_posts (
+                        id, source, social_data
+                    ) VALUES (
+                        :id, :source, :social_data
+                    )
+                    """,
+                    {
+                        'id': post.id,
+                        'source': post.source,
+                        'social_data': str(asdict(post.social_data)),
+                    },
+                )
+
+    def run(
+        self,
+        db_cursor_context: DatabaseConnection,
+        client,
+        transform_function: Callable[
+            [List[SocialMediaData]], List[SocialMediaData]
+        ],
+        id: str = 'dataengineering',
+        num_records: int = 100,
+    ):
+
+        logging.info('Running reddit pipeline.')
+        self.load(
+            social_data=self.transform(
+                social_data=self.extract(
+                    id=id, num_records=num_records, client=client
+                ),
+                transform_function=transform_function,
+            ),
+            db_cursor_context=db_cursor_context,
+        )
+
+
+class TwitterPipeline(SocialMediaPipeline):
+    @log_metadata
+    def extract(
+        self,
+        id: str,
+        num_records: int,
+        client: tweepy.API,
+    ) -> List[SocialMediaData]:
+        logging.info("Extracting twitter data.")
+        # if twitter client is None, raise an error
+        if client is None:
+            raise ValueError(
+                "twitter object is None. Please pass a valid tweepy.Tweet"
+                " object."
+            )
+
+        # given user name, get user id with tweepy
+        user_id = client.get_user(username=id).data.id
+        # get list of users the user_id is following with tweepy
+        user_ids_to_follow = [
+            str(u.id) for u in client.get_users_following(id=user_id).data
+        ]
+        start_time = (datetime.now() - timedelta(days=1)).strftime(
+            "%Y-%m-%dT%H:%M:%SZ"
+        )
+        list_of_tweets = [
+            client.get_users_tweets(
+                id=user_id,
+                exclude="retweets,replies",
+                start_time=start_time,
+                tweet_fields="id,text,author_id,created_at",
+            ).data
+            for user_id in user_ids_to_follow
+        ]
+        list_of_tweets = [t for t in list_of_tweets if t is not None]
+        # flatten list of tweets
+        list_of_tweets = [
+            tweet
+            for tweet_list in list_of_tweets
+            for tweet in tweet_list
+            if tweet_list is not None
+        ]
+        # convert list of tweets to list of TwitterTweetData
+        list_of_tweets = [
+            SocialMediaData(
+                id=tweet.id,
+                source='twitter',
+                social_data=TwitterTweetData(text=tweet.text),
+            )
+            for tweet in list_of_tweets
+        ]
+        return list_of_tweets[:num_records]
+
+    @log_metadata
+    def transform(
+        self,
+        social_data: List[SocialMediaData],
+        transform_function: Callable[
+            [List[SocialMediaData]], List[SocialMediaData]
+        ],
+    ) -> List[SocialMediaData]:
+
+        logging.info('Transforming twitter data.')
+        return transform_function(social_data)
+
+    @log_metadata
+    def load(
+        self,
+        social_data: List[SocialMediaData],
+        db_cursor_context: DatabaseConnection,
+    ) -> None:
+
+        logging.info('Loading twitter data.')
+        if db_cursor_context is None:
+            raise ValueError(
+                'db_cursor is None. Please pass a valid DatabaseConnection'
+                ' object.'
+            )
+
+        with db_cursor_context as cur:
+            for post in social_data:
+                cur.execute(
+                    """
+                    INSERT OR REPLACE INTO social_posts (
+                        id, source, social_data
+                    ) VALUES (
+                        :id, :source, :social_data
+                    )
+                    """,
+                    {
+                        'id': post.id,
+                        'source': post.source,
+                        'social_data': str(asdict(post.social_data)),
+                    },
+                )
+
+    def run(
+        self,
+        db_cursor_context: DatabaseConnection,
+        client,
+        transform_function: Callable[
+            [List[SocialMediaData]], List[SocialMediaData]
+        ],
+        id: str = 'startdataeng',
+        num_records: int = 100,
+    ):
+
+        logging.info('Running twitter pipeline.')
+        self.load(
+            social_data=self.transform(
+                social_data=self.extract(
+                    id=id, num_records=num_records, client=client
+                ),
+                transform_function=transform_function,
+            ),
+            db_cursor_context=db_cursor_context,
+        )
+
+
 def pipeline_factory(source: str) -> Tuple[praw.Reddit | tweepy.Client, SocialMediaPipeline]:
     factory = {
         'reddit': (
@@ -86,11 +306,11 @@ def pipeline_factory(source: str) -> Tuple[praw.Reddit | tweepy.Client, SocialMe
                 client_secret=os.environ['REDDIT_CLIENT_SECRET'],
                 user_agent=os.environ['REDDIT_USER_AGENT'],
             ),
-            RedditETL(),
+            RedditPipeline(),
         ),
         'twitter': (
             tweepy.Client(bearer_token=os.environ['BEARER_TOKEN']),
-            TwitterETL(),
+            TwitterPipeline(),
         ),
     }
     if source in factory:
