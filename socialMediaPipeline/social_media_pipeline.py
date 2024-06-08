0548831ac4f73e439df2d4b7770b138a05c236c9
unknown <autumnteo96@gmail.com>
2024-06-09 00:31:45 +0800

    added logging and function to build db schema

--- socialMediaPipeline/social_media_pipeline.py
+++ socialMediaPipeline/social_media_pipeline.py
@@ -8,7 +8,7 @@ from typing import Callable, List, Tuple
 import praw
 import tweepy
 from dotenv import load_dotenv
-from metadata import log_metadata
+from logging import log_metadata
 from utils.db import DatabaseConnection
 
 load_dotenv()
@@ -16,6 +16,16 @@ load_dotenv()
 
 @dataclass
 class RedditPostData:
+    """Dataclass to hold reddit post data.
+
+    Args:
+        title (str): Title of the reddit post.
+        score (int): Score of the reddit post.
+        url (str): URL of the reddit post.
+        comms_num (int): Number of comments on the reddit post.
+        created (str): Datetime (string repr) of when the reddit
+             post was created.
+    """
 
     title: str
     score: int
@@ -27,13 +37,22 @@ class RedditPostData:
 
 @dataclass
 class TwitterTweetData:
+    """Dataclass to hold twitter post data.
 
+    Args:
+        text (str): Text of the twitter post.
+    """
     text: str
 
 
 @dataclass
 class SocialMediaData:
+    """Dataclass to hold social media data.
 
+    Args:
+        id (str): ID of the social media post.
+        text (str): Text of the social media post.
+    """
     id: str
     source: str
     social_data: RedditPostData | TwitterTweetData
@@ -86,7 +105,15 @@ class RedditPipeline(SocialMediaPipeline):
         num_records: int,
         client: praw.Reddit,
     ) -> List[SocialMediaData]:
+        """Get reddit data from a subreddit.
 
+        Args:
+            id (str): Subreddit to get data from.
+            num_records (int): Number of records to get.
+
+        Returns:
+            List[RedditPostData]: List of reddit post data.
+        """
         logging.info('Extracting reddit data.')
         if client is None:
             raise ValueError(
@@ -122,7 +149,16 @@ class RedditPipeline(SocialMediaPipeline):
             [List[SocialMediaData]], List[SocialMediaData]
         ],
     ) -> List[SocialMediaData]:
+        """Function to transform reddit data, by only keeping the
+        posts with number of comments greater than 2 standard deviations
+        away from the mean number of comments.
 
+        Args:
+            social_data (List[RedditPostData]): List of reddit post data.
+
+        Returns:
+            List[RedditPostData]: Filtered list of reddit post data.
+        """
         logging.info('Transforming reddit data.')
         return transform_function(social_data)
 
@@ -132,6 +168,11 @@ class RedditPipeline(SocialMediaPipeline):
         social_data: List[SocialMediaData],
         db_cursor_context: DatabaseConnection,
     ) -> None:
+        """Function to load data into a database.
+
+        Args:
+            reddit_data (List[RedditPostData]): List of reddit post data.
+        """
         logging.info('Loading reddit data.')
         if db_cursor_context is None:
             raise ValueError(
@@ -166,7 +207,14 @@ class RedditPipeline(SocialMediaPipeline):
         id: str = 'dataengineering',
         num_records: int = 100,
     ):
+        """Function to run the ETL pipeline.
 
+        Args:
+            db_cursor_context (DatabaseConnection): Database connection.
+            client (praw.Reddit): Reddit client.
+            id (str): Subreddit to get data from.
+            num_records (int): Number of records to get.
+        """
         logging.info('Running reddit pipeline.')
         self.load(
             social_data=self.transform(
@@ -240,7 +288,16 @@ class TwitterPipeline(SocialMediaPipeline):
             [List[SocialMediaData]], List[SocialMediaData]
         ],
     ) -> List[SocialMediaData]:
+        """Function to transform twitter data, by only keeping the
+        posts with number of comments greater than 2 standard deviations
+        away from the mean number of comments.
 
+        Args:
+            social_data (List[SocialMediaData]): List of twitter post data.
+
+        Returns:
+            List[SocialMediaData]: Filtered list of twitter post data.
+        """
         logging.info('Transforming twitter data.')
         return transform_function(social_data)
 
@@ -250,7 +307,11 @@ class TwitterPipeline(SocialMediaPipeline):
         social_data: List[SocialMediaData],
         db_cursor_context: DatabaseConnection,
     ) -> None:
+        """Function to load data into a database.
 
+        Args:
+            social_data (List[SocialMediaData]): List of twitter post data.
+        """
         logging.info('Loading twitter data.')
         if db_cursor_context is None:
             raise ValueError(
@@ -285,7 +346,14 @@ class TwitterPipeline(SocialMediaPipeline):
         id: str = 'startdataeng',
         num_records: int = 100,
     ):
+        """Function to run the ETL pipeline.
 
+        Args:
+            db_cursor_context (DatabaseConnection): Database connection.
+            client (tweepy.Twitter): Twitter client.
+            id (str): Subreddit to get data from.
+            num_records (int): Number of records to get.
+        """
         logging.info('Running twitter pipeline.')
         self.load(
             social_data=self.transform(
